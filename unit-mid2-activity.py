df4e33577fc09e52d64577eaa1e869f1e330397c
Taylor <taylorjsayson@gmail.com>
2024-04-10 12:16:17 -0700

    did some of project

---
+++ unit-mid2-activity.py
@@ -0,0 +1,17 @@
+# Unit 2 Activity
+# Taylor Sayson
+# 10 April 2024 
+
+# Create a file that includes 
+# one custom function that has at least  one parameter and have a return value
+# at least one loop, for or while
+
+#  Create a bot that helps the user create study cards
+
+print("Hello, I am study bot! I can help you create study cards for any upcoming quiz or test")
+print("To begin, let's create a title for your study cards.")
+title = input("TITLE: ")
+
+print(f"Thank you, now we will add questions to your {title} study card set")
+print("Type your first question you want to add to your study card set")
+question1 = input("QUESTION 1: ")
