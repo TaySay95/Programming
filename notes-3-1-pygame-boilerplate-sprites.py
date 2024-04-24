6506bb868040a6bb210a63294740ae225fc44db6
Taylor <taylorjsayson@gmail.com>
2024-04-24 15:01:25 -0700

    pygame poilerplate moving dvd logos

--- notes-3-1-pygame-boilerplate-sprites.py
+++ notes-3-1-pygame-boilerplate-sprites.py
@@ -7,6 +7,119 @@
 
 
 import pygame
+import random
+
+
+def update(self):
+        #Update  position of Dvdlogo
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+
+    # Keep the Dvdlogo in the screen
+    # Right side of the screen
+    #     - if the right edge of dvdlogo > WIDTH
+    #          - switch the direction (+vel-x -> -vel-x)
+        if self.rect.right >= 1280:
+            self.vel_x = -self.vel_x
+        if self.rect.left <= 0:
+            self.vel_x = -self.vel_x
+        if self.rect.top <=0:
+            self.vel_y = -self.vel_y
+        if self.rect.bottom >=720:
+            self.vel_y = -self.vel_y
+
+        print(self.rect.x, self.rect.y)
+
+
+
+
+class Kanye(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./Images/Kanye-West-PNG.png")
+        self.rect = self.image.get_rect() 
+
+        self.image = pygame.transform.scale(self.image, (self.rect.width // 2, self.rect.height // 2))
+
+        self.rect.centerx = random.randrange(250,970)
+        self.rect.centery = random.randrange(250,470)
+
+        self.vel_x = random.randrange(-8, 8)
+        self.vel_y = random.randrange(-8, 8)
+
+    def update(self):
+                self.rect.x += self.vel_x
+                self.rect.y += self.vel_y
+
+                if self.rect.right >= 1280:
+                    self.vel_x = -self.vel_x
+                if self.rect.left <= 0:
+                    self.vel_x = -self.vel_x
+                if self.rect.top <=0:
+                    self.vel_y = -self.vel_y
+                if self.rect.bottom >=720:
+                    self.vel_y = -self.vel_y
+
+                print(self.rect.x, self.rect.y)
+
+
+class Rb(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./Images/rb.webp")
+        self.rect = self.image.get_rect() 
+
+        self.image = pygame.transform.scale(self.image, (self.rect.width // 2, self.rect.height // 2))
+
+        self.rect.centerx = random.randrange(200,1020)
+        self.rect.centery = random.randrange(200,520)
+
+        self.vel_x = random.randrange(-8, 8)
+        self.vel_y = random.randrange(-8, 8)
+
+    def update(self):
+                self.rect.x += self.vel_x
+                self.rect.y += self.vel_y
+
+                if self.rect.right >= 1280:
+                    self.vel_x = -self.vel_x
+                if self.rect.left <= 0:
+                    self.vel_x = -self.vel_x
+                if self.rect.top <=0:
+                    self.vel_y = -self.vel_y
+                if self.rect.bottom >=720:
+                    self.vel_y = -self.vel_y
+
+                print(self.rect.x, self.rect.y)
+
+class Mercedes(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./Images/mercedes.png")
+        self.rect = self.image.get_rect() 
+
+        self.image = pygame.transform.scale(self.image, (self.rect.width // 2, self.rect.height // 2))
+
+        self.rect.centerx = random.randrange(200,1020)
+        self.rect.centery = random.randrange(200,520)
+
+        self.vel_x = 1
+        self.vel_y = 1
+
+    def update(self):
+                self.rect.x += self.vel_x
+                self.rect.y += self.vel_y
+
+                if self.rect.right >= 1280:
+                    self.vel_x = -self.vel_x
+                if self.rect.left <= 0:
+                    self.vel_x = -self.vel_x
+                if self.rect.top <=0:
+                    self.vel_y = -self.vel_y
+                if self.rect.bottom >=720:
+                    self.vel_y = -self.vel_y
+
+                print(self.rect.x, self.rect.y)
 
 
 class Dvdlogo(pygame.sprite.Sprite):
@@ -18,6 +131,37 @@ class Dvdlogo(pygame.sprite.Sprite):
         # Sets the x and y to 0
         #   First position of the image is in the top right
         self.rect = self.image.get_rect()
+
+        self.rect.centerx = random.randrange(150,1130)
+        self.rect.centery = random.randrange(150,570)
+
+        # How much position changes over time
+        # - pixels per tick
+
+        self.vel_x = random.randrange(-8, 8)
+        self.vel_y = random.randrange(-8, 8)
+
+
+    def update(self):
+            #Update  position of Dvdlogo
+            self.rect.x += self.vel_x
+            self.rect.y += self.vel_y
+
+        # Keep the Dvdlogo in the screen
+        # Right side of the screen
+        #     - if the right edge of dvdlogo > WIDTH
+        #          - switch the direction (+vel-x -> -vel-x)
+            if self.rect.right >= 1280:
+                self.vel_x = -self.vel_x
+            if self.rect.left <= 0:
+                self.vel_x = -self.vel_x
+            if self.rect.top <=0:
+                self.vel_y = -self.vel_y
+            if self.rect.bottom >=720:
+                self.vel_y = -self.vel_y
+
+            print(self.rect.x, self.rect.y)
+    
 def start():
     """Environment Setup and Game Loop"""
 
@@ -32,6 +176,7 @@ def start():
     GREEN   = (  0, 255,   0)
     BLUE    = (  0,   0, 255)
     GRAY    = (128, 128, 128)
+    PURPLE  = (128,   0, 128)
 
     WIDTH   = 1280    # Pixels
     HEIGHT  =  720
@@ -43,14 +188,15 @@ def start():
     clock = pygame.time.Clock()
 
     dvdlogo = Dvdlogo()
-
-    #Move the DVD logo to the middle-ish
-    dvdlogo.rect.centerx = WIDTH// 2
-    dvdlogo.rect.centery = HEIGHT//2
+    kanye = Kanye()
+    rb = Rb()
+    mercedes = Mercedes()
 
     all_sprites = pygame.sprite.Group()
     all_sprites.add(dvdlogo)
-
+    all_sprites.add(kanye)
+    all_sprites.add(rb)
+    all_sprites.add(mercedes)
     pygame.display.set_caption("DVD Screen Saver")
 
     # --MAIN LOOP--
@@ -61,9 +207,10 @@ def start():
                 done = True
 
 		# --- Update the world state
+        all_sprites.update()
 
         # --- Draw items
-        screen.fill(BLACK)
+        screen.fill(PURPLE)
 
         all_sprites.draw(screen)
 
