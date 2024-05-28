abceb13a6ac68e7bcbea8a7c159ca784a0f9deb8
Taylor Sayson <taylorjsayson@gmail.com>
2024-05-27 21:46:14 -0700

    added skating physics

--- final.py
+++ final.py
@@ -27,52 +27,74 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pg.image.load("./Images/galaga_ship.png")
-        self.image = pg.transform.scale(
-            self.image, (self.image.get_width() // 2, self.image.get_height() // 2)
-        )
+        self.image = pg.Surface((70, 70))
+        self.image.set_colorkey(BLACK)
+
+        pg.draw.circle(self.image, BLUE, (35, 35), 35)
 
         self.rect = self.image.get_rect()
         # Initialize velocity
         self.vel_x = 0
         self.vel_y = 0
-
+        self.acceleration = 0.2
+        self.deceleration = 0.03
+        self.slowdecel = .01
     def update(self):
         """ Move the player. """
-
-        # Move left/right
+        pressed = pg.key.get_pressed()
+        
+        # Handle horizontal movement
+        if pressed[pg.K_LEFT]:
+            self.vel_x = max(self.vel_x - self.acceleration, -6)
+        elif pressed[pg.K_RIGHT]:
+            self.vel_x = min(self.vel_x + self.acceleration, 6)
+        else:
+            if self.vel_x >1:
+                self.vel_x = max(self.vel_x - self.deceleration, 0)
+            elif self.vel_x <1:
+                self.vel_x = min(self.vel_x + self.deceleration, 0)
+            elif self.vel_x > 0:
+                self.vel_x = max(self.vel_x - self.slowdecel, 0)
+            elif self.vel_x < 0:
+                self.vel_x = min(self.vel_x + self.slowdecel, 0)
+                
+        # Handle vertical movement
+        if pressed[pg.K_UP]:
+            self.vel_y = max(self.vel_y - self.acceleration, -6)
+        elif pressed[pg.K_DOWN]:
+            self.vel_y = min(self.vel_y + self.acceleration, 6)
+        else:
+            if self.vel_y > 1:
+                self.vel_y = max(self.vel_y - self.deceleration, 0)
+            elif self.vel_y < -1:
+                self.vel_y = min(self.vel_y + self.deceleration, 0)
+            elif self.vel_y > 0:
+                self.vel_y = max(self.vel_y - self.slowdecel, 0)
+            elif self.vel_y < 0:
+                self.vel_y = min(self.vel_y + self.slowdecel, 0)
+                
+                
         self.rect.x += self.vel_x
 
         # Move up/down
         self.rect.y += self.vel_y
-
-    # Player-controlled movement:
-    def go_left(self):
-        """ Called when the user hits the left designated button. """
-        self.vel_x = -6
-
-    def go_right(self):
-        """ Called when the user hits the right designated button. """
-        self.vel_x = 6
-
-    def go_up(self):
-        """ Called when the user hits the right designated button. """
-        self.vel_y = -6
-
-    def go_down(self):
-        """ Called when the user hits the right designated button. """
-        self.vel_y = 6
-
-    def stop_leftright(self):
-        """ Called when the user lets off the keyboard. """
-        self.vel_x = 0
-    def stop_updown(self):
-        """ Called when the user lets off the keyboard. """
-        self.vel_y = 0
-
+        
+        # Keep player within screen bounds
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            self.vel_x = 0
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.vel_x = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+            self.vel_y = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.vel_y = 0
+                 
 def start():
     """Environment Setup and Game Loop"""
-
     pg.init()
 
     # --Game State Variables--
@@ -88,7 +110,7 @@ def start():
     all_sprites.add(player)
 
 
-    pg.display.set_caption("Shoot 'Em Up")
+    pg.display.set_caption("GAME")
 
     # --Main Loop--
     while not done:
@@ -96,25 +118,7 @@ def start():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done = True
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_RIGHT:
-                    player.go_right()
-                if event.key == pg.K_LEFT:
-                    player.go_left()
-                if event.key == pg.K_UP:
-                    player.go_up()
-                if event.key == pg.K_DOWN:
-                    player.go_down()
-            if event.type == pg.KEYUP:
-                if event.key == pg.K_LEFT and player.vel_x < 0:
-                    player.stop_leftright()
-                if event.key == pg.K_RIGHT and player.vel_x > 0:
-                    player.stop_leftright()
-                if event.key == pg.K_UP and player.vel_y < 0:
-                    player.stop_updown()
-                if event.key == pg.K_DOWN and player.vel_y > 0:
-                    player.stop_updown()
-        # --- Update the world state
+
         all_sprites.update()
 
         # --- Draw items
