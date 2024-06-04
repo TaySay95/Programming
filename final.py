e5a724cd64e15ac4b2200760871e052d1d939be0
Taylor Sayson <taylorjsayson@gmail.com>
2024-06-04 02:42:37 -0700

    edited

--- final.py
+++ final.py
@@ -1,8 +1,5 @@
-#Taylor Sayson
+# Taylor Sayson
 # 13 May 2024
-
-# pygame-example-shmup.py
-# Shoot 'em Up
 
 import random
 import pygame as pg
@@ -32,52 +29,49 @@ class Ball(pg.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH // 2, HEIGHT // 2)
-        self.vel_x = 0
-        self.vel_y = 10
-        self.deceleration = 1
-        self.slowdecel = .01
+        self.vel_x = 8
+        self.vel_y = 0
+
 
         self.player_sprites = players
 
     def update(self):
         """ Move puck based on contact """
-        # collisions = pg.sprite.spritecollide(self, self.player_sprites, False)
-        # for x in collisions:
-        #     if isinstance(x, Player):
-        #         # Increase puck velocity to 15
-        #         self.vel_x = 15
-        #         self.vel_y = 15
 
-        # Decleration
-        # if self.rect.right > WIDTH:
-        #     self.rect.right = WIDTH
-        #     self.vel_x = self.vel_x * -1
-        # if self.rect.left < 0:
-        #     self.rect.left = 0
-        #     self.vel_x = self.vel_x * -1
-        if self.rect.y< HEIGHT:
-            self.vel_y = self.vel_y + 2
-        
-        if self.rect.bottom == 720:
-            self.vel_y = self.vel_y * -1
-        if self.rect.y < 720 and self.vel_y < 0:
-            self.vel_y =self.vel_y *-1 
+            
+        if self.rect.top < 0:
+            self.rect.top = 0  # keep it inside the screen
+            self.vel_y *= -1
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+            self.vel_y *= -1
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.vel_x *= -1
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            self.vel_x *= -1
+            
 
+        if self.vel_y >=0:
+            self.vel_y = self.vel_y + .5
+        if self.vel_y <0:
+            self.vel_y = self.vel_y + .65
+        if -0.5<self.vel_y<0.5 and self.rect.bottom ==HEIGHT:
+            self.vel_y *=0
+
+        if self.vel_x >1:
+            self.vel_x = max(self.vel_x - .017, 0)
+        elif self.vel_x <1:
+            self.vel_x = min(self.vel_x + .017, 0)
+        elif self.vel_x > 0:
+            self.vel_x = max(self.vel_x - .005, 0)
+        elif self.vel_x < 0:
+            self.vel_x = min(self.vel_x + .005, 0)
 
         self.rect.x += self.vel_x
         self.rect.y += self.vel_y
 
-
-
-        # if self.vel_x >0:
-        #     self.vel_x = self.vel_x - self.deceleration
-        # elif self.vel_x <0:
-        #     self.vel_x = self.vel_x + self.deceleration
-
-        # if self.vel_y >= 0:
-        #         self.vel_y = self.vel_y - self.deceleration
-        # if self.vel_y < 0:
-        #     self.vel_y = self.vel_y + .5
 
 
 class Player(pg.sprite.Sprite):
@@ -95,7 +89,7 @@ class Player(pg.sprite.Sprite):
         # Initialize velocity
         self.vel_x = 0
         self.vel_y = 10
-        self.acceleration = 0.5
+        self.acceleration = 0.8
         self.deceleration = 0.9
         self.slowaccel = 4
 
